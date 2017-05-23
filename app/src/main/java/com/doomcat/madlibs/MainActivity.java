bcba6907ff74516821db725cdeaaa83f06a72fd0
Alex Lee <doomcat9167@gmail.com>
2017-05-23 14:04:18 -0700

    add radio button, selecting radio button logs in android monitor. Need backend logic to route different stories

--- app/src/main/java/com/doomcat/madlibs/MainActivity.java
+++ app/src/main/java/com/doomcat/madlibs/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/doomcat/madlibs/OptionsActivity.java
@@ -0,0 +1,54 @@
+package com.doomcat.madlibs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    public static final String TAG = OptionsActivity.class.getSimpleName();
+    private Button mNextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        //Bind button
+        mNextButton = (Button) findViewById(R.id.nextButton);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(OptionsActivity.this, FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.happyOption:
+                if (checked)
+                    Log.d(TAG, "HAPPY clicked");
+                break;
+            case R.id.sadOption:
+                if (checked)
+                    Log.d(TAG, "SAD clicked");
+                break;
+            case R.id.brutalOption:
+                if (checked)
+                    Log.d(TAG, "BRUTAL clicked");
+                break;
+        }
+    }
+
+}
